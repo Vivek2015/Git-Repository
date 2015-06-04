d7db82346a1392858067c7e05c68c762bbb7baeb
Vivek2015 <viv007.s@hotmail.com>
2015-06-04 14:52:08 +0530

    Powershell Mcafee Service Report
    Mcafee Services running/stopped status in a domain will be  generated in
    excel with export-CSV

---
+++ Macfee_Status.ps1
@@ -0,0 +1,7 @@
+﻿$PCs=(Get-Content G:\list.txt)
+Invoke-Command -ComputerName $PCs -Credential admin {Get-Service -DisplayName "Mcafee*"|`
+Select-Object @{expression={$_.Name}; label="Services"},`
+              @{expression={$_.DisplayName}; label="Discreption"},`
+              status, PSComputerName}|`
+              Select-Object * -ExcludeProperty RunspaceId, PSShowComputerName|`
+              Export-Csv G:\mcafee_Status_$((Get-Date).ToString('dd-MM-yyyy')).csv -NoTypeInformation
